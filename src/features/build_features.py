77fabbee0f44f6cdbb67e244730036f4c7a9c81e
fyk7 <tfscofield115@gmail.com>
2021-02-07 17:08:02 +0900

    add description to build_features

---
+++ src/features/build_features.py
@@ -0,0 +1,5 @@
+'''
+In this project, preproccessing of features is done during model training(image augmentation), 
+so nothing is written in this file. 
+Usually, this file describes the preprocessing method of the features.
+'''
